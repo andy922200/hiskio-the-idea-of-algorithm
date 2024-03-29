bc9fe70a89ae792d6b01a9624fd7d52c83a7fe28
andy922200-m1 <andy922200@gmail.com>
2024-03-29 16:58:59 +0800

    feat: add 'parse' func in HashTable

--- app-ch8-23.js
+++ app-ch8-23.js
@@ -8,13 +8,27 @@ class HashTable {
         }
     }
 
+    parse(key){
+        let result = 0
+        for(let i=0; i<key.length; i++){
+            result += key.charCodeAt(i)
+        }
+        return result
+    }
+
     hashDivision(key) {
         return key % this.size
     }
 
     hashMulti(key) {
+        let parsedKey = key
+
+        if (typeof key === 'string') {
+            parsedKey = this.parse(key)
+        }
+
         const A = (Math.sqrt(5) - 1) / 2
-        return Math.floor(this.size * ((key * A) % 1))
+        return Math.floor(this.size * ((parsedKey * A) % 1))
     }
 
     set(key, value) {
@@ -41,10 +55,13 @@ const hashTable = new HashTable(6)
 hashTable.set(11424, 'Mike')
 hashTable.set(6254, 'James')
 hashTable.set(4171, 'Drake')
-hashTable.set(554, 'Kevin')
+hashTable.set('white', '#FFFFFF')
+hashTable.set('magenta', '#FF00FF')
+hashTable.set('red', '#FF0000')
 
 hashTable.printAll()
 
 console.log(hashTable.get(11424))
 console.log(hashTable.get(4171))
-console.log(hashTable.get(8888))
+console.log(hashTable.get('red'))
+console.log(hashTable.get('red').value)
